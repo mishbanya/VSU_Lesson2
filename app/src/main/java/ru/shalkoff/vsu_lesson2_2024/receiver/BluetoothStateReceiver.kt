b8d438c58be15e4e413a22ecf4a6e94fa4327264
Degtyarev111 <degtyarevmishas@yandex.ru>
2024-04-06 00:04:18 +0300

    fix

--- app/src/main/java/ru/shalkoff/vsu_lesson2_2024/receiver/BluetoothStateReceiver.kt
+++ app/src/main/java/ru/shalkoff/vsu_lesson2_2024/receiver/BluetoothStateReceiver.kt
@@ -6,9 +6,6 @@ import android.content.Context
 import android.content.Intent
 import android.widget.Toast
 
-//TODO 4
-// Почему не работает этот ресивер?
-// Сделайте так, чтобы он работал
 class BluetoothStateReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
